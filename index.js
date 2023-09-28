c584c9a5548947b3d01ca61497ffd72460a35d5b
Rohit Bharti <rohitbharti279@gmail.com>
2023-09-28 12:33:23 +0530

    dynamic page

--- index.js
+++ index.js
@@ -4,13 +4,30 @@ const path = require("path");
 const app = express();
 const publicPath = path.join(__dirname, 'public');
 
+app.set('view engine', 'ejs');
+
 // for home page (or say landing page) we have to create a file with "index.html" file name  
 
 // when we use express .static then we need to give the extension of the file name
 // app.use(express.static(publicPath));
 
-app.get('/', (req, res)=>{
+// if you r not using req then use _ instead of request
+app.get('/', (_, res)=>{
     res.sendFile(`${publicPath}/index.html`);
+})
+
+app.get("/profile", (req,res) => {
+    const user = {
+        name: "Rohit Bharti",
+        id: 510519041,
+        college: "IIEST",
+        skills: ["html", "css", "javascript", "php", 1,2]
+    }
+    res.render('profile', {user});
+})
+
+app.get("/login", (req,res) => {
+    res.render("login");
 })
 
 app.get('/anyname', (req, res)=>{
